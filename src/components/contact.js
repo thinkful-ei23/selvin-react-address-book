57374efae43aecb6c69dab6ae3c5d79908218829
Selvin Norman <selvin715@gmail.com>
2018-08-29 05:28:37 -0700

    removed ul-li

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,12 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+    <div className="contact">
+      <img  className="contact-photo" role="presentation" src={props.photo} />
+      <span className="contact-name">{props.name}</span>
+      <span className="contact-address">{props.address}</span>
+    </div>
+  )
+}
